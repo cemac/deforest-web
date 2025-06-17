f1ee1e1c2c903a741c031079d250e3d783c07f6b
Richard Rigby <r.rigby@leeds.ac.uk>
2025-06-17 11:04:00 +0100

    Add base layer and data type selections

--- js/site.js
+++ js/site.js
@@ -1,11 +1,9 @@
 'use strict';
 
+
 /** global variables: **/
 
-/* map objects: */
-var map = null;
-var map_title = null;
-var map_color_map = null;
+
 /* site variables: */
 var site_vars = {
   /* map element: */
@@ -24,8 +22,13 @@ var site_vars = {
   'data_url': 'data',
   'data_areas': ['africa', 'americas', 'se_asia'],
   'data_types': ['adm1', 'adm2'],
-  'data': {},
+  'data': {}
 };
+/* map objects: */
+var map = null;
+var map_title = null;
+var map_color_map = null;
+var map_data_groups = null;
 
 /* map mouse position overlay: */
 
@@ -81,7 +84,9 @@ L.control.mousePosition = function (options) {
     return new L.Control.MousePosition(options);
 };
 
+
 /** functions **/
+
 
 /* data loading function: */
 async function load_data() {
@@ -96,9 +101,9 @@ async function load_data() {
     for (var j = 0 ; j < site_vars['data_types'].length ; j++) {
       /* this type: */
       var data_type = site_vars['data_types'][j];
-      /* data for this area: */
-      site_vars['data'][data_area][data_type] = {};
-      var type_data = site_vars['data'][data_area][data_type];
+      /* data for this area and type: */
+      area_data[data_type] = {};
+      var type_data = area_data[data_type];
       /* load data from json using fetch: */
       var type_file = site_vars['data_url'] + '/' +
                       data_area + '_' + data_type + '.json';
@@ -183,112 +188,155 @@ function draw_color_map() {
 
 /* map loading function: */
 function load_map() {
-  /* cartodb map tiles: */
+  /* define cartodb layer: */
   var layer_cartodb = L.tileLayer(
     'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-      attribution: '',
+      'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      'subdomains': 'abcd',
       minZoom: 1,
       maxZoom: 10
     }
   );
-
-  /* define map: */
-  map = L.map('content_map', {
-    zoom: 2,
-    minZoom: 2,
-    maxZoom: 9,
-    layers: [],
-    center: [0, 0],
-    maxBounds: [
-      [-75, -300],
-      [75, 300]
-    ],
-    maxBoundsViscosity: 1.0,
-    zoomControl: false,
-    attributionControl: false
-  });
-
-  /* add layers to map: */
-  map.addLayer(layer_cartodb);
-
-  /* data type to plot: */
-  var data_type = 'adm1';
-
-  /* loop through data areas defined in site_vars: */
-  for (var i = 0 ; i < site_vars['data_areas'].length ; i++) {
-    /* this area: */
-    var data_area = site_vars['data_areas'][i];
-    /* data for this type + area: */
-    var type_data = site_vars['data'][data_area][data_type];
-    /* add polygons for this data type: */
-    for (var j = 0 ; j < type_data.length ; j ++) {
-      var poly_value = type_data[j]['dTnc'];
-      if (poly_value == 'null') {
-        continue;
-      };
-      var poly_npix = type_data[j]['npix'];
-      if ((poly_npix == 'null') || (parseInt(poly_npix) < 250)) {
-        continue;
-      };
-      var poly_sd = type_data[j]['sd'];
-      var poly_name = type_data[j]['name'];
-      var poly_fc = type_data[j]['forest_cover_2020'];
-      var poly_color = value_to_color(poly_value);
-      var poly = L.polygon(type_data[j]['geometry'], {'color': poly_color, 'weight': 1, 'fillColor': poly_color, 'fillOpacity': 0.6});
-      poly.bindTooltip(
-        '<b>name:</b> ' + poly_name + '<br>' +
-        '<b>npix:</b> ' + poly_npix + '<br>' +
-        '<b>dTnc:</b> ' + poly_value.toFixed(3) + ' (+/- ' + poly_sd.toFixed(3) + ')<br>' +
-        '<b>forest cover 2020:</b> ' + poly_fc.toFixed(3)
-      );
-      poly.addTo(map);
+  /* define openstreetmap layer: */
+  var layer_osm = L.tileLayer(
+    'https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      'attribution': '&copy; <a href="https://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+      minZoom: 1,
+      maxZoom: 10
+    }
+  );
+  /* all base tile layers: */
+  var tile_layers = {
+    'Carto': layer_cartodb,
+    'Open Street Map': layer_osm
+  };
+  /* define map if not defined: */
+  if (map == null) {
+    map = L.map('content_map', {
+      zoom: 2,
+      minZoom: 2,
+      maxZoom: 9,
+      layers: [],
+      center: [0, 0],
+      maxBounds: [
+        [-75, -300],
+        [75, 300]
+      ],
+      maxBoundsViscosity: 1.0,
+      zoomControl: false,
+      attributionControl: false
+    });
+    /* add base tile layer to map: */
+    map.addLayer(layer_cartodb);
+    /* add zoom control: */
+    var zoom_control = L.control.zoom();
+    zoom_control.addTo(map);
+    /* add map title: */
+    map_title = L.control();
+    map_title.onAdd = function(map) {
+       this._div = L.DomUtil.create('div', 'map_control map_title');
+       this.update();
+       return this._div;
     };
-  };
-
-  /* add colour map: */
-  var color_map_src = draw_color_map();
-  map_color_map = L.control({position: 'bottomright'});
-  map_color_map.onAdd = function(map) {
-    this._div = L.DomUtil.create('div', 'map_control map_color_map');
-      this.update(color_map_src);
-      return this._div;
-  };
-  map_color_map.update = function(color_map_html) {
-    this._div.innerHTML = color_map_html;
-  };
-  map_color_map.addTo(map);
-
-  /* add zoom control: */
-  var zoom_control = L.control.zoom();
-  zoom_control.addTo(map);
-
-  /* add map title: */
-  map_title = L.control();
-  map_title.onAdd = function(map) {
-     this._div = L.DomUtil.create('div', 'map_control map_title');
-     this.update();
-     return this._div;
-  };
-  map_title.update = function(title) {
-    if (title != undefined) {
-      this._div.innerHTML = title;
+    map_title.update = function(title) {
+      if (title != undefined) {
+        this._div.innerHTML = title;
+      };
     };
+    map_title.addTo(map);
+    /* add base layer control: */
+    L.control.layers(
+      tile_layers,
+      null,
+      {'collapsed': true, 'position': 'topleft'}
+    ).addTo(map);
+    /* init layer groups for data types: */
+    map_data_groups = {};
+    var data_layers = {};
+    /* loop through data types: */
+    for (var i = 0 ; i < site_vars['data_types'].length ; i++) {
+      /* this type: */
+      var data_type = site_vars['data_types'][i];
+      /* create layer group: */
+      map_data_groups[data_type] = L.layerGroup([]);
+      map_data_groups[data_type].title = data_type;
+      data_layers[data_type] = map_data_groups[data_type];
+      /* update map title when layer is added: */
+      map_data_groups[data_type].on('add', (e) => {
+        map_title.update(e.target.title);
+      });
+      /* loop through data areas defined in site_vars: */
+      for (var j = 0 ; j < site_vars['data_areas'].length ; j++) {
+        /* this area: */
+        var data_area = site_vars['data_areas'][j];
+        /* data for this area: */
+        var area_data = site_vars['data'][data_area];
+        /* data for this area and type: */
+        var type_data = area_data[data_type];
+        /* create polygons for this data type: */
+        for (var k = 0 ; k < type_data.length ; k++) {
+          /* ignore areas where dt value is not defined: */
+          var poly_value = type_data[k]['dTnc'];
+          if (poly_value == 'null') {
+            continue;
+          };
+          /* ignore areas without sufficient pixel counts: */
+          var poly_npix = type_data[k]['npix'];
+          if ((poly_npix == 'null') || (parseInt(poly_npix) < 250)) {
+            continue;
+          };
+          /* get required values for the area: */
+          var poly_sd = type_data[k]['sd'];
+          var poly_name = type_data[k]['name'];
+          var poly_fc = type_data[k]['forest_cover_2020'];
+          var poly_color = value_to_color(poly_value);
+          var poly = L.polygon(type_data[k]['geometry'], {'color': poly_color, 'weight': 1, 'fillColor': poly_color, 'fillOpacity': 0.6});
+          poly.bindTooltip(
+            '<b>name:</b> ' + poly_name + '<br>' +
+            '<b>npix:</b> ' + poly_npix + '<br>' +
+            '<b>dTnc:</b> ' + poly_value.toFixed(3) + ' (+/- ' + poly_sd.toFixed(3) + ')<br>' +
+            '<b>forest cover 2020:</b> ' + poly_fc.toFixed(3),
+            {'sticky': true, 'offset': [3, -3]}
+          );
+          /* add polygon to layer group: */
+          map_data_groups[data_type].addLayer(poly);
+        };
+      };
+      /* if this is the first layer group, add to map: */
+      if (i == 0) {
+        map_data_groups[data_type].addTo(map);
+      };
+    };
+    /* add data type control: */
+    L.control.layers(
+      data_layers,
+      null,
+      {'collapsed': true, 'position': 'topright'}
+    ).addTo(map);
+    /* add mouse pointer position: */
+    L.control.mousePosition().addTo(map);
+    /* add scale bar: */
+    L.control.scale().addTo(map);
   };
-  map_title.addTo(map);
-  /* update map title: */
-  var my_title = 'adm1';
-  map_title.update(my_title);
-
-  /* add mouse pointer position: */
-  L.control.mousePosition().addTo(map);
-
-  /* add scale bar: */
-  L.control.scale().addTo(map);
-
+  /* add colour map if not yet added: */
+  if (map_color_map == null) {
+    var color_map_src = draw_color_map();
+    map_color_map = L.control({position: 'bottomright'});
+    map_color_map.onAdd = function(map) {
+      this._div = L.DomUtil.create('div', 'map_control map_color_map');
+        this.update(color_map_src);
+        return this._div;
+    };
+    map_color_map.update = function(color_map_html) {
+      this._div.innerHTML = color_map_html;
+    };
+    map_color_map.addTo(map);
+  };
 };
 
 
 /** add listeners: **/
+
 
 /* on page load: */
 window.addEventListener('load', function() {
